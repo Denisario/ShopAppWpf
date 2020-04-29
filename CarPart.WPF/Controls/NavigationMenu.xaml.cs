e8163c421d520368dd8b858ce7cc798e7ece56d0
Denis Shestopalov <KROTIK96@gmail.com>
2020-04-29 22:46:18 +0300

    Added nav menu

---
+++ CarPart.WPF/Controls/NavigationMenu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarPart.WPF.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для NavigationMenu.xaml
+    /// </summary>
+    public partial class NavigationMenu : UserControl
+    {
+        public NavigationMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCloseMenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            UserData.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonOpenMenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            UserData.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+    }
+}
